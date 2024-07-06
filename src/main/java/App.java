2ee32a8d16d8f61c83e746bb45b9df2c6aa8ea53
RAJU <79203757+rajubhovi1111111111@users.noreply.github.com>
2024-07-07 02:04:19 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,8 +4,6 @@ public class App
     {
         System.out.println("I am your awesome Java application!");
 
-        DeepThought ourSuperComputer = new DeepThought();
-        int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
-        System.out.println(ans);
+
     }
 }
